fb083569ade75728f0a34cf59b9cfd6f2ad748bb
Judit Novak <judit.novak@gmail.com>
2025-03-14 15:03:12 +0100

    Simple changes applied after the 2nd round of review

--- src/nvim/eval/funcs.c
+++ src/nvim/eval/funcs.c
@@ -7648,7 +7648,6 @@ static void get_xdg_var_list(const XDGVarType xdg, typval_T *rettv)
     }
   } while (iter != NULL);
   xfree(dirs);
-  xfree((char *)appname);
 }
 
 /// "stdpath(type)" function
